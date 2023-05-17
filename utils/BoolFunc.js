59a0890453557ae0bf3c1a115e687bd2936ba012
imkzuma <edu.gungkrisna@gmail.com>
2023-05-18 01:20:59 +0800

    [fixed] fixed logic for handling umkm

--- utils/BoolFunc.js
+++ utils/BoolFunc.js
@@ -1,4 +1,7 @@
 export const isUmkmOpen = (rangeHours) => {
+    if(rangeHours === 24){
+        return true;
+    } 
     const now = new Date();
     const [start, end] = rangeHours.replace(/[^0-9.-]+/g,"").split('-').map(time => {
         const [hours, minutes] = time.split('.');
