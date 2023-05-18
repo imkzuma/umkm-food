8b7a3640b73bd0d354c41f1dfab08a26a2665806
imkzuma <edu.gungkrisna@gmail.com>
2023-05-18 16:29:13 +0800

    [FIXED] Fixed responsive layout and add logo

--- components/Landing/Hero/Hero.js
+++ components/Landing/Hero/Hero.js
@@ -1,4 +1,5 @@
-import { Box, Flex, Stack, Text } from "@chakra-ui/react";
+import { LogoOnlyColored } from "@/components/Logo";
+import { Box, Flex, Heading, Image, Stack, Text } from "@chakra-ui/react";
 
 export default function HeroComponent(){
     return(
@@ -15,14 +16,33 @@ export default function HeroComponent(){
                 bgSize={'cover'}
                 bgPosition={'center center'}
             >
-                <Stack direction = {'column'} textAlign={'center'} maxW={'500px'}>
-                    <Text fontSize={'5xl'}>
-                        LOGO
-                    </Text>
-                    <Text fontSize = {'40px'} color = "white" fontWeight={'600'}>
+                <Stack 
+                    direction = {'column'} 
+                    justifyContent={'center'}
+                    alignItems={'center'} 
+                    textAlign={'center'} 
+                    maxW={{md:'600px'}}
+                    spacing = {4}
+                    px = {{
+                        base: '15px',
+                        md: 0
+                    }}
+                >
+                    <LogoOnlyColored boxSize={'4rem'} />
+
+                    <Heading 
+                        fontWeight={'bold'} 
+                        size={{ base: 'xl', md: '2xl' }} 
+                        color = "white"
+                    >
                         Hungry? Just Order it
-                    </Text>
-                    <Text fontSize={'17px'} fontWeight={'400'} color = "white">
+                    </Heading>
+
+                    <Text 
+                        fontSize={{ base: 'md', md: 'lg' }} 
+                        fontWeight={'400'} 
+                        color = "white"
+                    >
                         Order your favorite meals here, on our web. Same fast delivery. Countless restos to try. 
                         Enjoy dining from home.
                     </Text>
